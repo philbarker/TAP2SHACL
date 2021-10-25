aac0e330ff6c77a4709d52d86ebb156b062dfae0
Phil Barker <phil.barker@pjjk.co.uk>
2021-10-25 14:15:16 +0100

    add dependencies

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@
 from distutils.core import setup
 
 setup(name='TAP2SHACL',
-      version='0.1',
+      version='0.1.1',
       description='DC TAP to SHACL cnoverter',
       author='Phil Barker',
       author_email='phil.barker@pjjk.co.uk',
       url='https://github.com/philbarker/TAP2SHACL',
-      packages=['TAP2SHACL'],
+      packages=['tap2shacl'],
+      dependency_links=[
+        "git+https://github.com/philbarker/AP2SHACL#egg=ap2shacl"
+        "git+https://github.com/philbarker/APClasses#egg=AP"
+        "git+https://github.com/philbarker/TAP2AP#egg=TAP2AP"
+        "https://github.com/dcmi/dctap-python/archive/main.zip"
+      ]
      )
