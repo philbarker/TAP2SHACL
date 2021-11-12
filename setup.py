05fb9e9450cb645c50f5ac922a8281893b73a3c6
Phil Barker <phil.barker@pjjk.co.uk>
2021-11-12 14:46:36 +0000

    update tests for how booleans in shapeInfo are handled by AP

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="TAP2SHACL",
-    version="0.2.1",
+    version="0.2.2",
     description="DC TAP to SHACL cnoverter",
     author="Phil Barker",
     author_email="phil.barker@pjjk.co.uk",
