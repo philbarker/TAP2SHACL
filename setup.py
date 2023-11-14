2aaed9031f14c1ea8cb2ea93c49dd473e59fb845
Phil Barker <phil.barker@pjjk.co.uk>
2023-11-14 12:47:15 +0000

    bump ver

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="TAP2SHACL",
-    version="0.3.0",
+    version="0.3.1",
     description="DC TAP to SHACL converter",
     author="Phil Barker",
     author_email="phil.barker@pjjk.co.uk",
