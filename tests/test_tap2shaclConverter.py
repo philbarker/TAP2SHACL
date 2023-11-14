2544e09b0ea24b06cb239a18f31a111448929c2a
Phil Barker <phil.barker@pjjk.co.uk>
2023-11-14 12:46:39 +0000

    Upgrade how rdf:type is handled.

--- tests/test_tap2shaclConverter.py
+++ tests/test_tap2shaclConverter.py
@@ -49,4 +49,6 @@ def test_convertTAP2SHACL(test_Converter):
     expected_sg = Graph().parse(shaclFileName)
     c.convertAP2SHACL()
     for stmt in expected_sg:
+        if stmt not in expected_sg:
+            print(stmt)
         assert stmt in c.sg
