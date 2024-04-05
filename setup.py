d1c7db5a8f95e6bda1e7a1ce8feb6903bc90c6bf
Phil Barker <phil.barker@pjjk.co.uk>
2024-04-05 16:05:44 +0100

    Changing dependency to fixed versions of AP related packages

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="TAP2SHACL",
-    version="0.5.0",
+    version="1.0.0-alpha.1",
     description="DC TAP to SHACL converter",
     author="Phil Barker",
     author_email="phil.barker@pjjk.co.uk",
