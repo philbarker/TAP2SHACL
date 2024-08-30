0f2a4ebb2460ecc5e9f8f857d8c7efffb6198735
Phil Barker <phil.barker@pjjk.co.uk>
2024-08-30 15:03:52 +0100

    fixing version

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 
 setup(
-    name="TAP2SHACL",
-    version="1.0.0-alpha.1",
+    name="tap2shacl",
+    version="1.0.0-alpha.2",
     description="DC TAP to SHACL converter",
     author="Phil Barker",
     author_email="phil.barker@pjjk.co.uk",
