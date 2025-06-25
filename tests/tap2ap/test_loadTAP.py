978460bc08a08968b911430e9151afc3cf72a2f7
Phil Barker <phil.barker@pjjk.co.uk>
2025-06-25 12:19:18 +0100

    Adding messages about properties

--- tests/tap2ap/test_loadTAP.py
+++ tests/tap2ap/test_loadTAP.py
@@ -33,6 +33,7 @@ def test_loadTAP():
     assert sh0Constraints[0]["propertyLabel"] == "Title"
     assert sh0Constraints[0]["note"] == "In English."
     assert sh0Constraints[0]["propertyDescription"] == "Title of the book."
+    assert sh0Constraints[0]["message"] == "Incorrect data for title."
     assert sh0Constraints[1]["propertyID"] == "dct:creator"
     assert sh0Constraints[2]["propertyID"] == "sdo:isbn"
     assert sh0Constraints[3]["propertyID"] == "rdf:type"
